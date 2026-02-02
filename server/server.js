20a988e8a4bd40200e786d2a70a18549fb16a070
phongluong8901 <phongluong3366@gmail.com>
2026-02-02 15:00:21 +0700

    e1

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+require('dotenv').config()
+
+const app = express()
+const port = process.env.PORT || 8888
+
+app.use(express.json())
+app.use(express.urlencoded({extends: true}))
+
+app.use('/', (req, res) => {
+    res.send("Server ON")
+})
+
+app.listen(port, () => {
+    console.log('Server runing on the porrt ' + port)
+})
